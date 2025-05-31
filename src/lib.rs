ea65d203c038c95b589ffa46f95a23597ee6a6a2
Sabbyasachi <chakrabortysabbyasachi@gmail.com>
2025-05-31 08:11:48 +0530

    feat(client): initialize client package with TypeScript and Jest configuration
    - Added package.json for client with dependencies and scripts for testing.
    - Created tsconfig.json with compiler options for TypeScript, enabling strict mode and bundler settings.
    
    feat(rust): implement basic Solana program logic
    
    - Added lib.rs for a Solana program that manages on-chain data with a counter.
    - Implemented process_instruction function to handle account data and update the counter.

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
+};
+entrypoint!(process_instruction);
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+struct OnChainData {
+    count: u32,
+}
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    // Your logic here
+
+    let mut iter = accounts.iter();
+    let data_account = next_account_info(&mut iter)?;
+    let mut counter = OnChainData::try_from_slice(&data_account.data.borrow_mut())?;
+    if (counter.count == 0) {
+        counter.count = 1;
+    } else {
+        counter.count *= 2;
+    }
+    counter.serialize(&mut *data_account.data.borrow_mut());
+    Ok(())
+}
